b1acb9804ecf03f698b87791479c0b6e41fd6a1b
Juliana Marques <marquesdjuliana@gmail.com>
2023-05-12 19:10:07 -0300

    Req.: 02 criando um header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email, wallet } = this.props;
     return (
-      <div>Header!</div>
+      <div>
+        <div>
+          <h2 data-testid="email-field">{`Email: ${email}`}</h2>
+        </div>
+        <div>
+          <h2 data-testid="total-field">{`Despesa total: R$ ${wallet}`}</h2>
+          <h2 data-testid="header-currency-field">BRL</h2>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  wallet: state.wallet.total,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  wallet: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
